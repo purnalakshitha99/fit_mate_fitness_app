2463dc148a309b0277ce4b02a2022f685f2181e8
IT21158254 <IT21158254@my.sliit.lk>
2024-04-15 10:42:16 +0530

    image added

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Gym3 from "../assets/gym2.jpg";
+
+const Login = () => {
+  return (
+    <div>
+      <img className="w-full" src={Gym3} alt="Gym" /> {/* Added w-full class */}
+    </div>
+  );
+};
+
+export default Login;
+
