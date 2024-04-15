e498df556b0c57f3d77ba432099876909058a138
purnalakshitha99 <purnalakshitha2@gmail.com>
2024-04-15 11:22:19 +0530

    change the basic backend structure

--- backend/fitmate-api/src/main/java/fitmate_api/FitmateApiApplication.java
+++ backend/fitmate-api/src/main/java/fitmate_api/FitmateApiApplication.java
@@ -1,13 +1,13 @@
-package com.example.backend;
+package fitmate_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class FitmateApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(FitmateApiApplication.class, args);
 	}
 
 }
