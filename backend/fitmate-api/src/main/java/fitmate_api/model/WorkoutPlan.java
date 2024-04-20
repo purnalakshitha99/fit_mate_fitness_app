54ecc41a6f6743efc0e88bd1ee71b3450fadad3e
it21157950 <it21157950@my.sliit.lk>
2024-04-20 18:21:11 +0530

    implemented the create part

--- backend/fitmate-api/src/main/java/fitmate_api/model/WorkoutPlan.java
+++ backend/fitmate-api/src/main/java/fitmate_api/model/WorkoutPlan.java
@@ -9,26 +9,10 @@ import java.sql.Timestamp;
 @Entity
 @Table(name = "WorkoutPlans")
 public class WorkoutPlan {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "plan_id")
-    private Long Id;
-
-    @ManyToOne
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
-
-    @Column(name = "plan_details", nullable = false)
-    private String planDetails;
-
-    @Column(name = "created_at")
-    private Timestamp createdAt;
-
-    @Column(name = "updated_at")
-    private Timestamp updatedAt;
-
-
-
+    private Long id;
     private String workOutGoals ;
     private Integer age ;
     private String gender;
