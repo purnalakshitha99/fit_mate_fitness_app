f26132f33e092ae9176752cace8fa671a485f45b
IT21185298 <it21185298@my.sliit.lk>
2024-04-20 14:31:44 +0530

    WorkoutStatusDto file create

---
+++ backend/fitmate-api/src/main/java/fitmate_api/DTO/WorkoutStatusDTO.java
@@ -0,0 +1,16 @@
+package fitmate_api.DTO;
+
+import lombok.Data;
+
+import java.time.LocalTime;
+import java.util.Date;
+
+@Data
+public class WorkoutStatusDTO {
+    private String typeOfWorkout;
+    private Date date;
+    private LocalTime time;
+    private Integer numberOfPushUp;
+
+
+}
---
+++ backend/fitmate-api/src/main/java/fitmate_api/controller/WorkoutStatusController.java
@@ -0,0 +1,12 @@
+package fitmate_api.controller;
+
+import jakarta.annotation.Resources;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+
+public class WorkoutStatusController {
+
+}
