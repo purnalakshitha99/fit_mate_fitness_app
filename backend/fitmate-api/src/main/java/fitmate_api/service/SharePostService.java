c2c7f977b8ce69d81381d5515eeba955e9d98798
IT21158254 <IT21158254@my.sliit.lk>
2024-05-06 17:09:39 +0530

    comment controller, dto, model created, user can comment post now

--- backend/fitmate-api/src/main/java/fitmate_api/service/SharePostService.java
+++ backend/fitmate-api/src/main/java/fitmate_api/service/SharePostService.java
@@ -1,10 +1,15 @@
 package fitmate_api.service;
 
 import fitmate_api.DTO.SharePostDTO;
+import fitmate_api.model.SharePost;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface SharePostService {
     ResponseEntity<Object> sharePost(SharePostDTO sharePostDTO);
+
+    List<SharePost> getAll();
 }
